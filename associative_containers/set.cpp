5068400cf00187b1ab53a03742cfaa301f2a0636
sivaramakrishnan1998 <sivaposhac1998@gmail.com>
2021-04-02 12:54:54 +0530

    revisiting stl

--- associative_containers/set.cpp
+++ associative_containers/set.cpp
@@ -1,35 +1,64 @@
-#include<iostream>
-#include<set>
+#include <iostream>
 using namespace std;
 
-template <class T>
-void print(T begind) {
 
-	for(auto no: begind) {
-		cout << no << '\n';
-	}
-}
+#include <set>
 
 int main() {
-	
-	set<int> s;
-	s.insert(10);
-	s.insert(7);
-	s.insert(10);
-	s.insert(71);
-	s.insert(17);
-	s.insert(8);
+    set<int> s;
+    int arr[] = {10,122,11,233,25,1,1,2,3};
+    int size  = sizeof(arr)/sizeof(int);
+    for (int i=0; i<size; i++) {
+        // insertion
+        s.insert(arr[i]);
+    }
+    for (set<int>::iterator it=s.begin();it!=s.end();it++) {
+        cout << *it << " ";
+    }
+    cout << '\n';
 
-	s.erase(8);
+    // erase
+    s.erase(122);
 
-	// search
-	auto f = s.find(17);
+    // find the iterator of the element to be deleted
+    auto it = s.find(2);
+    s.erase(it);
 
-	if (f!=s.end()) {
-		cout << "17 exists \n";
-	}
-	
-	print(s);
+    for (set<int>::iterator it=s.begin();it!=s.end();it++) {
+        cout << *it << " ";
+    }
 
-	return 0;
+
+    // Implementing dictionary
+    set<pair<int,int>> dict ;
+
+    dict.insert(make_pair(10,20));
+    dict.insert(make_pair(10,30));
+    dict.insert(make_pair(20,30));
+    dict.insert(make_pair(20,40));
+    dict.insert(make_pair(30,30));
+    dict.insert(make_pair(40,10));
+    dict.insert(make_pair(50,80));
+    dict.insert(make_pair(45,90));
+    dict.insert(make_pair(20,50));
+
+    cout << "\nBefore insertion / deletion\n";
+
+
+    for (auto ptr:dict){
+        cout << "key " << ptr.first << " value " << ptr.second << '\n';
+    }
+
+
+    dict.erase(dict.find(make_pair(10,30)));
+
+    dict.insert(make_pair(10,40));
+
+    cout << "\nAfter inser  tion / deletion\n";
+    for (auto ptr:dict){
+        cout << "key " << ptr.first << " value " << ptr.second << '\n';
+    }
+
+
+    return 0;
 }
